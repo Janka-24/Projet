768a5374f824deb4c18a19d40b92ab5a2fe65cde
Kajan <kajanm2006@gmail.com>
2024-04-23 11:36:32 +0200

    correction

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/authentification",
     plugins: [react()],
+    base: "/authentification/"
 })
